296e1b3e6f046b94aeb3154813768fe3a3f8f72f
wx <wx@wxdeMacBook-Pro.local>
2020-11-30 22:03:38 +0800

    feat: 二叉树中序遍历

---
+++ 94.二叉树的中序遍历.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// var inorderTraversal = function(root, result=[]) {
+//   if(root) {
+//     inorderTraversal(root.left, result)
+//     result.push(root.val)
+//     inorderTraversal(root.right, result)
+//   }
+//   return result
+// };
+var inorderTraversal = function(root) {
+  const result = []
+  const stack = []
+  while(root || stack.length) {
+    while(root) {
+      stack.push(root)
+      root = root.left
+    }
+    root = stack.pop()
+    result.push(root.val)
+    root = root.right
+  }
+  return result
+};
+// @lc code=end
+
